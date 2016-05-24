ef47687c27ba32d4bdbbf2ee35b782ebc63132ce
simpleoneman <bogdan.kischak@gmail.com>
2016-05-24 14:01:37 +0300

    Add keydown

--- js/common.js
+++ js/common.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-  
+
+  //BODIA start
+  var MOVE_TOP = 38;
+  var MOVE_RIGHT = 39;
+  var MOVE_DOWN = 40;
+  var MOVE_LEFT = 37;
+
+  $(document.body).keydown(function(key){
+
+    switch (key.keyCode) {
+      case MOVE_TOP: { alert('top'); break; }
+      case MOVE_RIGHT: { alert('right'); break; }
+      case MOVE_DOWN: { alert('down'); break; }
+      case MOVE_LEFT: { alert('left'); break; }
+    }
+
+  });
+  //BODIA end
+
+  //ANDRIY start
+
+  //ANDRIY end
+
+  //ROMAN start
+
+  //ROMAN end
+
+  //NASTIA start
+
+  //NASTIA end
+
+  //IRA start
+
+  //IRA end
+
 });
